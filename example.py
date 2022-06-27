08749d335ae8326256b5de2fa3c29ac9f0963de6
Mark Drury <Mark_18_flash@yahoo.co.uk>
2022-06-27 17:05:29 +0100

    Added python notesfolder

--- example.py
+++ example.py
@@ -1,1 +1,10 @@
+from random import random
+
+
 print ("Hello world")
+
+
+
+def age_predictor():
+    random.range(0, 100)
+    print
---
+++ python_notes/recap.py
@@ -0,0 +1,23 @@
+#print("this is my file")
+
+#comment in python
+#can also select a block of code and hit forwardslash
+#integer = whole number
+#floating point = decimal number
+#string = text characters requires speech marks
+#none = null
+#Python does not need to decale data types
+
+
+greeting = "Alreet world" #string variable
+print (greeting)
+
+apples = 12
+banana = 10
+
+sum = apples + banana
+
+def Add():
+    print(sum)
+
+
